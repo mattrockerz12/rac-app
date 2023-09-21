e5ebf0a319eb0ca826325a54fc189a934d44edd1
mattrockerz12 <mattrockerz35@gmail.com>
2023-09-21 18:45:43 +0800

    Scroll added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import Cta from "./components/Cta";
@@ -8,19 +9,50 @@ import Header from "./components/Header";
 import Service from "./components/Service";
 import Slider from "./components/Slider";
 import MenuContextProvider from "./context/menu-context";
+import { Link as ScrollLink } from "react-scroll";
 
 function App() {
+  const [scrollTop, setScrollTop] = useState(false);
+
+  const handleScrollTop = () => {
+    if (window.scrollY > 70) {
+      setScrollTop(true);
+    } else if (window.scrollY < 70) {
+      setScrollTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollTop);
+    return () => {
+      window.removeEventListener("scroll", handleScrollTop);
+    };
+  }, [scrollTop]);
+
   return (
-    <MenuContextProvider>
-      <Header />
-      <Slider />
-      <Feature />
-      <About />
-      <Service />
-      <Cta />
-      <FunFact />
-      <Gallery />
-    </MenuContextProvider>
+    <>
+      <MenuContextProvider>
+        <Header />
+        <Slider />
+        <Feature />
+        <About />
+        <Service />
+        <Cta />
+        <FunFact />
+        <Gallery />
+      </MenuContextProvider>
+      {scrollTop === true ? (
+        <ScrollLink
+          to="wrapper"
+          smooth={true}
+          duration={500}
+          id="backToTop"
+          className="scroll-to-top scroll-to-target"
+        >
+          <i className="fa fa-angle-up"></i>
+        </ScrollLink>
+      ) : null}
+    </>
   );
 }
 
